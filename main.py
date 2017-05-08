149f437214246983e6ab933fa60b0a6be0b01656
EbbeS <stubbeebbe@gmail.com>
2017-05-08 14:19:09 +0200

    exponential increase?

--- main.py
+++ main.py
@@ -5,3 +5,48 @@ Created on Sun May  7 10:02:42 2017
 @author: Ebbe
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.integrate as integrate
+import math
+plt.close()
+p0 = [0,0]
+p1 = [3,4]
+L = math.sqrt((p1[0]-p0[0])**2 + (p1[1] - p0[1])**2)
+m = 1
+g = 9.82
+
+
+def deriv(var,t):
+    x1,vx1,y1,vy1 = var
+    ax1 = g*((x1-p0[0])/L)/m
+    ay1 = -g*((y1-p0[1])/L)/m
+    return [vx1,ax1,vy1,ay1]
+        
+t = np.arange(0, 25.0, 0.01)
+init = [p1[0],0,p1[1],0]
+z = integrate.odeint(deriv,init,t)
+plt.plot(t,z[:,1])
+plt.show()
+print(z)
+'''
+pi = np.pi
+sqrt = np.sqrt
+cos = np.cos
+sin = np.sin
+
+def deriv_z(z, phi):
+    u, udot = z
+    return [udot, -u + sqrt(u)]
+
+phi = np.linspace(0, 7.0*pi, 2000)
+zinit = [1.49907, 0]
+z = integrate.odeint(deriv_z, zinit, phi)
+u, udot = z.T
+# plt.plot(phi, u)
+fig, ax = plt.subplots()
+ax.plot(1/u*cos(phi), 1/u*sin(phi))
+ax.set_aspect('equal')
+plt.grid(True)
+plt.show()
+'''
