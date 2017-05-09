b2671e92932895757dff2ceb746dc1f7aa79dfbe
ebbestubbe <stubbe.ebbe@gmail.com>
2017-05-09 14:49:29 +0200

    single pendulum

--- main.py
+++ main.py
@@ -11,24 +11,56 @@ import scipy.integrate as integrate
 import math
 plt.close()
 p0 = [0,0]
-p1 = [3,4]
-L = math.sqrt((p1[0]-p0[0])**2 + (p1[1] - p0[1])**2)
-m = 1
+#L = math.sqrt((p1[0]-p0[0])**2 + (p1[1] - p0[1])**2)
+L = 10
 g = 9.82
 
+def deriv_z(var,t): #var:[theta,d(theta)/dt]
+    
+    dy0 = var[1]
+    dy1 = -g/L*math.sin(var[0])
+    return [dy0,dy1]
+
+t = np.arange(0,25.0,0.1)
+init = [1,0]
+z = integrate.odeint(deriv_z,init,t)
+plt.figure()
+plt.plot(t,z[:,0])
+plt.figure()
+plt.plot(t,z[:,1])
+x = -L*np.sin(z[:,0])
+y = -L*np.cos(z[:,0])
+plt.figure()
+plt.plot(x,y)
+plt.axis([-1.1*L,1.1*L,-1.1*L,1.1*L])
+plt.show()
+
+'''
 
 def deriv(var,t):
     x1,vx1,y1,vy1 = var
-    ax1 = g*((x1-p0[0])/L)/m
-    ay1 = -g*((y1-p0[1])/L)/m
+    #theta measured from vertical line to string in positive rotation
+    costheta = (y1-p0[1])/L
+    sintheta = (x1-p0[0])/L
+    ax1 = -g*(costheta)
+    ay1 = g*(sintheta)
     return [vx1,ax1,vy1,ay1]
         
-t = np.arange(0, 25.0, 0.01)
+t = np.arange(0, 5.0, 0.01)
 init = [p1[0],0,p1[1],0]
 z = integrate.odeint(deriv,init,t)
-plt.plot(t,z[:,1])
+plt.figure()
+plt.plot(t,z[:,0])
 plt.show()
-print(z)
+plt.figure()
+plt.plot(t,z[:,2])
+plt.show()
+
+
+plt.figure()
+plt.plot(z[:,0],z[:,2])
+#print(z)
+'''
 '''
 pi = np.pi
 sqrt = np.sqrt
