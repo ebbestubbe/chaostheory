aef8eb3cd6bb3505aa23fd7b57520716c4dcd679
EbbeS <stubbeebbe@gmail.com>
2017-05-23 22:00:20 +0200

    Doing animation

--- main.py
+++ main.py
@@ -3,55 +3,191 @@
 Created on Sun May  7 10:02:42 2017
 
 @author: Ebbe
+
 """
 
-import sys
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
-
-fig, ax = plt.subplots()
-fig.set_tight_layout(True)
-
-# Query the figure's on-screen size and DPI. Note that when saving the figure to
-# a file, we need to provide a DPI for that separately.
-print('fig size: {0} DPI, size in inches {1}'.format(
-    fig.get_dpi(), fig.get_size_inches()))
-
-# Plot a scatter that persists (isn't redrawn) and the initial line.
-x = np.arange(0, 20, 0.1)
-ax.scatter(x, x + np.random.normal(0, 3.0, len(x)))
-line, = ax.plot(x, x - 5, 'r-', linewidth=2)
-
-def update(i):
-    label = 'timestep {0}'.format(i)
-    print(label)
-    # Update the line and the axes (with a new xlabel). Return a tuple of
-    # "artists" that have to be redrawn for this frame.
-    line.set_ydata(x - 5 + i)
-    ax.set_xlabel(label)
-    return line, ax
-
-if __name__ == '__main__':
-    # FuncAnimation will call the 'update' function for each frame; here
-    # animating over 10 frames, with an interval of 200ms between frames.
-    anim = FuncAnimation(fig, update, frames=np.arange(0, 10), interval=200)
-    #if len(sys.argv) > 1 and sys.argv[1] == 'save':
-    #anim.save('line.gif', dpi=80, writer='imagemagick')
-    anim.save('basic_animation.mp4', fps=30)
-    plt.show()
-    #else:
-        # plt.show() will just loop the animation forever.
-    #    
-
-
-'''
+from numpy import sin, cos
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.integrate as integrate
-from matplotlib.animation import FuncAnimation
-import math
+import matplotlib.animation as animation
 
+G = 9.8  # acceleration due to gravity, in m/s^2
+L1 = 1.0  # length of pendulum 1 in m
+L2 = 1.0  # length of pendulum 2 in m
+M1 = 1.0  # mass of pendulum 1 in kg
+M2 = 1.0  # mass of pendulum 2 in kg
+
+
+def derivs(state, t):
+
+    dydx = np.zeros_like(state)
+    dydx[0] = state[1]
+
+    del_ = state[2] - state[0]
+    den1 = (M1 + M2)*L1 - M2*L1*cos(del_)*cos(del_)
+    dydx[1] = (M2*L1*state[1]*state[1]*sin(del_)*cos(del_) +
+               M2*G*sin(state[2])*cos(del_) +
+               M2*L2*state[3]*state[3]*sin(del_) -
+               (M1 + M2)*G*sin(state[0]))/den1
+
+    dydx[2] = state[3]
+
+    den2 = (L2/L1)*den1
+    dydx[3] = (-M2*L2*state[3]*state[3]*sin(del_)*cos(del_) +
+               (M1 + M2)*G*sin(state[0])*cos(del_) -
+               (M1 + M2)*L1*state[1]*state[1]*sin(del_) -
+               (M1 + M2)*G*sin(state[2]))/den2
+
+    return dydx
+
+# create a time array from 0..100 sampled at 0.05 second steps
+dt = 0.01
+t = np.arange(0.0, 10, dt)
+
+# th1 and th2 are the initial angles (degrees)
+# w10 and w20 are the initial angular velocities (degrees per second)
+th1 = 120.0
+w1 = -10.0
+th2 = -10.0
+w2 = 0.0
+
+# initial state
+state = np.radians([th1, w1, th2, w2])
+#color = '0.2'
+color = [0.3,0.3,0.3]
+# integrate your ODE using scipy.integrate.
+y = integrate.odeint(derivs, state, t)
+
+x1 = L1*sin(y[:, 0])
+y1 = -L1*cos(y[:, 0])
+
+x2 = L2*sin(y[:, 2]) + x1
+y2 = -L2*cos(y[:, 2]) + y1
+
+#fig = plt.figure(figsize=(15,15))
+fig = plt.figure()
+ax = fig.add_subplot(111, autoscale_on=False, xlim=(-2, 2), ylim=(-2, 2))
+ax.grid()
+plt.gca().set_aspect('equal', adjustable='box')
+
+line, = ax.plot([], [], 'o-',color=color, lw=2)
+time_template = 'time = %.1fs'
+time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
+#sub1 = fig.add_subplot(1,1,1)
+#sub1.xaxis.set_visible(False)
+#sub1.yaxis.set_visible(False)
+#sub1.set_xlim([-2, 2])
+#sub1.set_ylim([-2, 2])
+#sub1.axis('off')
+
+def init():
+    line.set_data([], [])
+    time_text.set_text('')
+    return line, time_text
+
+
+def animate(i):
+    thisx = [0, x1[i], x2[i]]
+    thisy = [0, y1[i], y2[i]]
+
+    line.set_data(thisx, thisy)
+    time_text.set_text(time_template % (i*dt))
+    return line, time_text
+
+ani = animation.FuncAnimation(fig, animate, np.arange(1, len(y)),
+                              interval=25, blit=True, init_func=init)
+
+ani.save('double_pendulum.mp4', fps=15)
+
+fig2 = plt.figure()
+ax1 = fig2.add_subplot(221)
+ax1.plot(t,y[:,0],color=color)
+ax2 = fig2.add_subplot(222)
+ax2.plot(t,y[:,1],color=color)
+ax3 = fig2.add_subplot(223)
+ax3.plot(t,y[:,2],color=color)
+ax4 = fig2.add_subplot(224)
+ax4.plot(t,y[:,3],color=color)
+
+print("done")
+plt.show()
+
+'''
+import matplotlib
+matplotlib.use("Agg") # configure backend for PNGs
+import matplotlib.pyplot as plt # make figures
+from matplotlib import patches # polygon shapes
+import math # square roots and other calculations
+import imageio # save images
+import scipy # rotation calculations
+import random 
+import os # file deletion
+import scipy.integrate as integrate
+import numpy as np
+
+def main():
+    fig = plt.figure(figsize=(30, 30))
+    plt.subplots_adjust(hspace=0, wspace=0)
+    
+    
+    #L = math.sqrt((p1[0]-p0[0])**2 + (p1[1] - p0[1])**2)
+    L = 1
+    g = 9.82
+    
+    def deriv_z(var,t): #var:[theta,d(theta)/dt]
+        
+        dy0 = var[1]
+        dy1 = -g/L*math.sin(var[0])
+        return [dy0,dy1]
+    
+    t = np.arange(0,5.0,0.05)
+    init = [1,0]
+    z = integrate.odeint(deriv_z,init,t)
+    x = -L*np.sin(z[:,0])
+    y = -L*np.cos(z[:,0])
+    n_images = len(t)
+    xdata = []
+    ydata = []
+    for i in range(n_images):
+        xdata.append([0,x[i]])
+        ydata.append([0,y[i]])
+    generateimages(fig,xdata,ydata)
+    
+    
+    images = [] #  Turn a list of images into a GIF using ImageIO
+    tag = "experiment2"
+    for n in range(n_images):
+        readname = str('temp' + repr(n) + '.png')
+        images.append(imageio.imread(readname))
+    
+    savegif(images,tag)    
+def generateimages(fig,xdata,ydata):
+    
+    for i in range(len(xdata)): # for every frame in the GIF
+        print("generating",i)
+        sub1 = fig.add_subplot(1,1,1)
+        sub1.xaxis.set_visible(False)
+        sub1.yaxis.set_visible(False)
+        sub1.set_xlim([-2, 2])
+        sub1.set_ylim([-2, 2])
+        sub1.axis('off')
+        line, = sub1.plot(xdata[i],ydata[i], '.-', lw=1)
+        
+        savename = str('temp' + repr(i) + '.png')
+        fig.savefig(savename, bbox_inches='tight', pad_inches=0, dpi=50)
+        plt.clf() 
+
+def savegif(images,tag):
+    
+    imageio.mimsave(str(tag) + '.gif', images,fps=60)
+    print("saved")
+    plt.close('all')
+if __name__ == "__main__":
+    print("start")
+    main()
+'''
+'''
 p0 = [0,0]
 #L = math.sqrt((p1[0]-p0[0])**2 + (p1[1] - p0[1])**2)
 L = 10
@@ -94,50 +230,5 @@ def update(i):
 anim = FuncAnimation(fig, update, frames=np.arange(0,len(t)), interval=2)
 #anim.save('line.gif', dpi=80, writer='imagemagick')
 plt.show()
-'''
-'''
-def deriv(var,t):
-    x1,vx1,y1,vy1 = var
-    #theta measured from vertical line to string in positive rotation
-    costheta = (y1-p0[1])/L
-    sintheta = (x1-p0[0])/L
-    ax1 = -g*(costheta)
-    ay1 = g*(sintheta)
-    return [vx1,ax1,vy1,ay1]
-        
-t = np.arange(0, 5.0, 0.01)
-init = [p1[0],0,p1[1],0]
-z = integrate.odeint(deriv,init,t)
-plt.figure()
-plt.plot(t,z[:,0])
-plt.show()
-plt.figure()
-plt.plot(t,z[:,2])
-plt.show()
 
-
-plt.figure()
-plt.plot(z[:,0],z[:,2])
-#print(z)
-'''
-'''
-pi = np.pi
-sqrt = np.sqrt
-cos = np.cos
-sin = np.sin
-
-def deriv_z(z, phi):
-    u, udot = z
-    return [udot, -u + sqrt(u)]
-
-phi = np.linspace(0, 7.0*pi, 2000)
-zinit = [1.49907, 0]
-z = integrate.odeint(deriv_z, zinit, phi)
-u, udot = z.T
-# plt.plot(phi, u)
-fig, ax = plt.subplots()
-ax.plot(1/u*cos(phi), 1/u*sin(phi))
-ax.set_aspect('equal')
-plt.grid(True)
-plt.show()
 '''
